54572d1f74b8905c8cd44a777bae9bb5ad52e8e4
Leonard Follner <Leonard.Follner@gmail.com>
2015-11-26 11:32:01 +0100

    [TASK] settings toggle now

--- BubbleGenerator.js
+++ BubbleGenerator.js
@@ -1,3 +1,11 @@
+function toggle(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+
 function init() {
 
   var canvas = document.getElementById('bubbleCanvas');
